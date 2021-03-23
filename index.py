6097294fe31cf29fc90ef5a7e33f977a62e1f348
dataNotAvailable <cruz.axl@gmail.com>
2021-03-23 11:36:24 -0400

    Added project files

--- index.py
+++ index.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-		return 'Web App with Python Flask!'
+	name = 'Axl'
+	return render_template("index.html",title="Welcome",username=name)
 
-app.run(host='0.0.0.0',port=81)
+
